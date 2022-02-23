eb7bc55fe5f0487fb6ffd26292310e25a53c0c81
Jessica <jessica@jmcooper.net>
2022-02-23 14:48:57 -0700

    commit

--- interp_ICAIRD/run_fv.py
+++ interp_ICAIRD/run_fv.py
@@ -1,16 +1,17 @@
-import torch
 import wandb
-from icaird_models import *
-
 import numpy as np
-import torch.nn as nn
-
 import torch.optim as optim
 import pandas as pd
 import os
 import argparse
 import random
 import torchvision.transforms.functional as TF
+import torch
+from utils.utils import *
+from datasets.dataset_generic import Generic_MIL_Dataset
+from models.model_clam import CLAM_SB, CLAM_MB
+from models.model_mil import MIL_fc, MIL_fc_mc
+from models.resnet_custom import resnet50_baseline
 
 pd.set_option('display.max_columns', None)
 
@@ -29,6 +30,8 @@ def set_seed(seed):
 
 
 set_seed(0)
+
+import torch.nn as nn
 
 
 parser = argparse.ArgumentParser()
@@ -50,7 +53,8 @@ parser.add_argument('--blur', default=False, type=bool)
 parser.add_argument('--normalise', default=False, type=bool)
 parser.add_argument('--sgd', default=False, type=bool)
 parser.add_argument('--relu', default=False, type=bool)
-
+parser.add_argument('--ckpt', default='../results/Endometrial/Results_Feb2022/Endo_all_classes_CLAM_sb_small')
+parser.add_argument('--model_type', default='CLAM_SB')
 
 
 def normalise(x):
@@ -71,15 +75,16 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 labels = ['malignant', 'insufficient', 'other_benign']
 
-att_model = CLAM_SB(n_classes=len(labels), subtyping=True)
+att_model = eval(args.model_type)(n_classes=len(labels))
 
-ckpt_path = 's_0_checkpoint.pt'
+ckpt_path = args.ckpt
 ckpt = torch.load(ckpt_path, map_location=torch.device('cpu'))
+
 ckpt_clean = {}
 for key in ckpt.keys():
     ckpt_clean.update({key.replace('3', '2'): ckpt[key]})
 
-att_model.load_state_dict(ckpt_clean, strict=True)
+att_model.load_state_dict(ckpt_clean)
 
 att_model.relocate()
 att_model.eval()
